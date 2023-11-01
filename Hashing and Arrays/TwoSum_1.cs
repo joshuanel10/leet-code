d29e4c8cf8a22e4074dad4ad4f6e05666758e5ad
joshuanel10 <12660812+joshuanel10@users.noreply.github.com>
2023-11-01 12:28:42 +0200

    Update TwoSum_1.cs
    Added Efficient Method

--- Hashing and Arrays/TwoSum_1.cs
+++ Hashing and Arrays/TwoSum_1.cs
@@ -1,3 +1,5 @@
+//Brute Force Method
+
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
     
@@ -9,5 +11,24 @@ public class Solution {
             }
         }
         throw new ArgumentException("No Solution Found");
+    }
+}
+
+//Efficient Method
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> numToIndex = new Dictionary<int, int>();
+        
+        for (int i = 0; i < nums.Length; i++) {
+            int complement = target - nums[i];
+            
+            if (numToIndex.ContainsKey(complement)) {
+                return new int[] { numToIndex[complement], i };
+            }
+            
+            numToIndex[nums[i]] = i; // Store the current number and its index in the Dictionary
+        }
+        
+        throw new ArgumentException("No solution found");
     }
 }
