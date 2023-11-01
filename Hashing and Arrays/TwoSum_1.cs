777a91dab9f3e6eaa408f3c2bd20cc975d59068c
joshuanel10 <12660812+joshuanel10@users.noreply.github.com>
2023-11-01 12:26:38 +0200

    Update TwoSum_1.cs
    Fixed Code

--- Hashing and Arrays/TwoSum_1.cs
+++ Hashing and Arrays/TwoSum_1.cs
@@ -1,27 +1,13 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
     
-    //List<int> output = new List<int>();
-    HashSet<int> myhash1 = new HashSet<int>();
-    bool check;
-    
-
         for (int i = 0; i < nums.Length; i++) {
-            
-            for (int j = 1; j < nums.Length; j++) {
-                if ((nums[i] + nums[j]) == target) {
-                    myhash1.Add(i);
-                    myhash1.Add(j);
-                    
+            for (int j = i + 1; j < nums.Length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
                 }
             }
         }
-    
-        
-    List<int> output= myhash1.ToList();    
-    int[] intPut = output.ToArray();
-    
-
-    return intPut;
-}
+        throw new ArgumentException("No Solution Found");
+    }
 }
